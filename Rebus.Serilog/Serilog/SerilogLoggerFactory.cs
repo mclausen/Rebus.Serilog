1ba0bf9944e7378ea5998a5d7a3e2ee23d4f212b
Martin Humlund Clausen <mc@nabufit.com>
2017-05-13 22:09:09 +0200

    Enriched logging statements with exception in cases where exceptions where parsed implcitly

--- Rebus.Serilog/Serilog/SerilogLoggerFactory.cs
+++ Rebus.Serilog/Serilog/SerilogLoggerFactory.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Rebus.Config;
+using Rebus.Extensions;
 using Rebus.Logging;
 using Serilog;
 
@@ -34,17 +39,41 @@ namespace Rebus.Serilog
 
             public void Debug(string message, params object[] objs)
             {
-                _logger.Debug(message, objs);
+                var exception = ExtractException(objs);
+                if (exception == null)
+                {
+                    _logger.Debug(message, objs);
+                }
+                else
+                {
+                    _logger.Debug(exception, message, objs);
+                }
             }
 
             public void Info(string message, params object[] objs)
             {
-                _logger.Information(message, objs);
+                var exception = ExtractException(objs);
+                if (exception == null)
+                {
+                    _logger.Information(message, objs);
+                }
+                else
+                {
+                    _logger.Information(exception, message, objs);
+                }
             }
 
             public void Warn(string message, params object[] objs)
             {
-                _logger.Warning(message, objs);
+                var exception = ExtractException(objs);
+                if (exception == null)
+                {
+                    _logger.Warning(message, objs);
+                }
+                else
+                {
+                    _logger.Warning(exception, message, objs);
+                }
             }
 
             public void Error(Exception exception, string message, params object[] objs)
@@ -54,7 +83,33 @@ namespace Rebus.Serilog
 
             public void Error(string message, params object[] objs)
             {
-                _logger.Error(message, objs);
+                var exception = ExtractException(objs);
+                if (exception == null)
+                {
+                    _logger.Warning(message, objs);
+                }
+                else
+                {
+                    _logger.Warning(exception, message, objs);
+                }
+            }
+
+            private static Exception ExtractException(params object[] objs)
+            {
+                if (objs == null || objs.Length <= 0) return null;
+
+                var exceptions = objs
+                    .Where(obj => obj.GetType() == typeof(Exception) || obj.GetType().GetTypeInfo().IsSubclassOf(typeof(Exception)))
+                    .ToList();
+
+                var exeptionsCount = exceptions.Count();
+                if (exeptionsCount == 0)
+                    return null;
+
+                if (exeptionsCount == 1)
+                    return exceptions.SingleOrDefault() as Exception;
+
+                return new AggregateException((IEnumerable<Exception>) exceptions);
             }
         }
     }
